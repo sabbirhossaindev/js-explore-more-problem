e2c44ac4606a1b738d477022ae69c3a7a2596542
sabbir hossain <mdsabbir477470@gmail.com>
2022-08-08 00:36:37 +0600

    slice explore

---
+++ splice.js
@@ -0,0 +1,10 @@
+/*
+ 1. array er modda kisu dita nita gala slice use korta hoba 
+ 2. slice use korla kno array data cut hoya jaba na.
+ 3. slice kaj kra start to end er aga prjento.
+
+*/
+
+const friends = [32, 45, 32, 22, 44, 62, 29, 69, 87];
+const partial = friends.slice(2, 5); // slice kaj kra start to end er aga projonto;
+console.log(partial);
