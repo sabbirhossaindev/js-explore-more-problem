7a78d565738dd35dc9b170c5a99cc532b757e99c
sabbir hossain <mdsabbir477470@gmail.com>
2022-08-08 02:45:14 +0600

    wood explore

---
+++ wood.js
@@ -0,0 +1,22 @@
+/* 
+fixed: per item wood requirements 
+1. chair --> 3 cft
+2. table --> 10 cft
+3. bed --> 50 cft
+vary: quantity
+*/
+
+function woodCalculator(chairQuantity, tableQuantity, bedQuantity ) {
+    const perChairWood = 3;
+    const perTableWood = 10;
+    const perBedWood = 5;
+
+    const chairWood = chairQuantity * perBedWood;
+    const tableWood = tableQuantity * perTableWood;
+    const bedWood = bedQuantity * perBedWood;
+
+    const totalWood = chairWood + tableWood + bedWood;
+    return totalWood;
+}
+const totalWood = woodCalculator(2, 3, 5);
+console.log('total wood : ',totalWood);
