237cb74ba1f9fc388db7187de7abf59b86d0f96b
sabbir hossain <mdsabbir477470@gmail.com>
2022-08-08 03:29:11 +0600

    card total sum  explore

---
+++ simple-card.js
@@ -0,0 +1,16 @@
+const shoppingCart = [
+    { name: 'shoe', price: 1200 },
+    { name: 'shirt', price: 2200 },
+    { name: 'pant', price: 3700 },
+    { name: 'belt', price: 600 }
+];
+function totalCost(products) {
+    let sum = 0;
+    for (let i = 0; i < products.length; i++) {
+        const product = products[i];
+        sum = sum + product.price;
+    }
+    return sum;
+} 
+const expense = totalCost(shoppingCart);
+console.log(expense);  
